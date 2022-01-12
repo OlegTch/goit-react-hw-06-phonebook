a36bc2787c5779bd37b54284a8817f769527489a
OlegTch <Olgard984@gmail.com>
2022-01-12 18:27:51 +0200

    fixed

--- src/components/ContactList/ContactItem.js
+++ src/components/ContactList/ContactItem.js
@@ -1,6 +1,12 @@
+import { useDispatch } from 'react-redux';
+import { contactsActions } from '../../redux/phonebook/phonebook-actions';
 import styles from './ContactList.module.css';
 
-const ContactItem = ({ id, name, number, onDeleteContact }) => {
+const ContactItem = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+
+  const onDeleteContact = idx => dispatch(contactsActions.deleteContact(idx));
+
   return (
     <li className={styles.contactsItem} id={id}>
       <p className={styles.contactsName}>{name}:</p>
@@ -8,7 +14,7 @@ const ContactItem = ({ id, name, number, onDeleteContact }) => {
       <button
         className={styles.contactsButton}
         type="button"
-        onClick={onDeleteContact}
+        onClick={() => onDeleteContact(id)}
       >
         Delete
       </button>
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,40 +1,18 @@
 import ContactItem from './ContactItem';
-import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
-import { contactsActions } from '../../redux/phonebook/phonebook-actions';
+import { useSelector } from 'react-redux';
 import { getFilteredContacts } from '../../redux/phonebook/phonebook-selector';
 import styles from './ContactList.module.css';
 
 const ContactList = () => {
   const contacts = useSelector(getFilteredContacts);
 
-  const dispatch = useDispatch();
-
-  const onDeleteContact = id => dispatch(contactsActions.deleteContact(id));
-
   return (
     <ul className={styles.contactsList}>
       {contacts.map(({ id, name, number }) => (
-        <ContactItem
-          key={id}
-          name={name}
-          number={number}
-          onDeleteContact={() => onDeleteContact(id)}
-        />
+        <ContactItem key={id} id={id} name={name} number={number} />
       ))}
     </ul>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      name: PropTypes.string,
-      number: PropTypes.string,
-    }),
-  ),
-  onDeleteContact: PropTypes.func,
 };
 
 export default ContactList;
